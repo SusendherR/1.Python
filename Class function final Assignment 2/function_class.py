98afad0521b2bea0e1172f9225d6a39f7619d914
SusendherR <162545463+SusendherR@users.noreply.github.com>
2024-04-04 17:05:01 +0530

    Add files via upload

---
+++ Class function final Assignment 2/function_class.py
@@ -0,0 +1,34 @@
+class classfunction1():
+    def Buy_inputs():
+        name=input("ENTER YOUR NAME:")
+        age=int(input("ENTER THE AGE:"))
+        name=input("ENTER THE SCHOOL NAME:")
+        name=input("ENTER THE DRGREE:")
+
+    def Multiplication():    
+        a= 63
+        b= 97
+        Mul = 6111
+
+    def len():
+        list2=[10,20,14,55,43,87,76]
+        print(list2)
+        print('Number of item in the list2:',len(list2))
+
+    def password():
+        if(name<="Hope"):
+            password=input("enter the password:")
+        if(password=="HOPE@123"):
+            print("Your password is correct:")
+        else:
+            print("Your password is incorrect:")
+
+    def marriage_elegibility():
+        Gender=input("Your Gender:")
+        age=int(input("Your Age:"))
+        if(age<=21):
+            print("NOT ELIGIBLE")
+        elif(age<18):
+            print("NOT ELIGIBLE")
+        else:
+            print("ELIGIBLE")
